24307983d87eecf10c09e1be295a1b966a0339c2
baggiponte <lucabaggi.uni@gmail.com>
2021-01-09 18:23:10 +0100

    added population data

--- README.md
+++ README.md
@@ -2,10 +2,20 @@
 
 This is Orizzonti Politici's open data repo about covid-19 vaccinations. The project is still a work in progress and we shall frequently update (and even overhaul) the structure of the repo.
 
-As of now, we are just manually retrieving the data by Italy's official vaccine data repository. Sources will be added soon.
+As of now, we are just manually retrieving the data by Italy's [official vaccine data repository](https://github.com/italia/covid19-opendata-vaccini).
 
 ## Structure
 
-The directory `data` contains the data we plan to automatically update. The script we use to retrieve the data shall be `get-data.R`.
+The directory `data` contains the data we plan to automatically update. The script we use to retrieve the data shall be `get-data.R`. The data is taken from  the official Italian repo, as we wrote above.
 
 Other notebooks are attempts of visualising and manipulating the data.
+
+## Credits
+
+One can never stop praising enough [OnData](https://ondata.it/). They are at the heart of the open data activism in Italy and have contributed enormously in demanding machine-readable open data and making it available to many of us.
+
+In particular, as of now we are using (or planning to) the following data they made available:
+
+* Italy's shapefiles in the EU's official [NUTS format](https://github.com/ondata/nuts) (Italian only).
+	* This very same data is also available on [another repository](https://github.com/ondata/covid19italia) of theirs: `covid19italia/risorse/fileGeografici` (Italian only).
+* Population data comes from the same repo as the one immediately above, but the file is `covid19italia/webservices/vaccini/risorse/popolazioneRegioni`.
--- get-data.R
+++ get-data.R
@@ -20,7 +20,7 @@ vaccininations_demographic_data <- read_csv(url_vaccininations_demographic_data)
   mutate(across(c(area, fascia_anagrafica), as.factor))
 
 vaccininations_demographic_data %>%
-  write.csv('data/ita_vaccinations_demographic_data.csv', row.names = F)
+  write.csv('data/ita_vaccinations_per_age_range.csv', row.names = F)
 
 # Aggregate Vaccinations Data ####
 
@@ -32,33 +32,60 @@ vaccinations_aggregated_data <-
   relocate(totale, .after = area)
 
 vaccinations_aggregated_data %>%
-  write.csv('data/ita_vaccinations_aggregated_data.csv', row.names = F)
+  write.csv('data/ita_vaccinations_aggregated.csv', row.names = F)
 
-# Cumulative Data ####
+# Cumulative Vaccination Data ####
 
 vaccinations_cumulative_italy <-
   vaccinations_aggregated_data %>%
   group_by(area) %>%
   summarise(totale = sum(totale)) %>%
   add_row(area = 'ITA',
-          totale = sum(vaccinations_aggregated_data$totale))
+          totale = sum(.$totale)) # the `.` indicates the file itself!
 
+# Create two new columns for cumulative data ####
+
+# group_by of vaccine deliveries data
 cumulative_vaccine_deliveries <-
   vaccine_deliveries_ita %>%
   group_by(area) %>%
   summarise(totale_dosi = sum(numero_dosi)) %>%
   add_row(
     area = 'ITA',
-    totale_dosi = sum(.$totale_dosi) # the `.` indicates the file itself!
+    totale_dosi = sum(.$totale_dosi) 
   )
 
+# retrieve regional population data thanks to OnData:
+
+url_regions_population <-
+  'https://raw.githubusercontent.com/ondata/covid19italia/master/webservices/vaccini/risorse/popolazioneRegioni.csv'
+
+regions_population <-
+  read_csv(url_regions_population) %>%
+  # order them before making an unorthodox merge
+  arrange(Name) %>%
+  bind_cols(
+    # since they are both sorted, add this column from another dataset
+    cumulative_vaccine_deliveries[1:21, 1]
+  ) %>%
+  select(area, OBS_VALUE) %>%
+  add_row(
+    area= 'ITA',
+    OBS_VALUE = sum(.$OBS_VALUE)
+  ) %>%
+  rename(popolazione_2020 = OBS_VALUE)
+
+
+# append vaccine deliveries data
 vaccinations_cumulative_italy %>%
-  inner_join(cumulative_vaccine_deliveries) %>%
+  inner_join(cumulative_vaccine_deliveries, by = 'area') %>%
+  inner_join(regions_population, by = 'area') %>%
   rename(
     vaccinazioni_eseguite = totale,
     dosi_consegnate = totale_dosi
   ) %>%
   mutate(
-    percentuale_somministrata = vaccinazioni_eseguite / dosi_consegnate
+    percentuale_somministrata = round(vaccinazioni_eseguite / dosi_consegnate, digits = 2),
+    vaccinati_su_centomila = round(vaccinazioni_eseguite / popolazione_2020, digits = 3) * 100000
   ) %>%
-  write.csv('data/ita_vaccinations_cumulative_total.csv', row.names = F)
+  write.csv('data/ita_vaccinations_cumulative.csv', row.names = F)
