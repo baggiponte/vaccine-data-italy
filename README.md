e026df027f85dc44a3d8e916aa089ffcf67b36ca
baggiponte <lucabaggi.uni@gmail.com>
2021-01-09 16:20:31 +0100

    renamed csv files

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-basic EDA and group_by
+# COVID-19 Vaccine Data
+
+This is Orizzonti Politici's open data repo about covid-19 vaccinations. The project is still a work in progress and we shall frequently update (and even overhaul) the structure of the repo.
+
+As of now, we are just manually retrieving the data by Italy's official vaccine data repository. Sources will be added soon.
+
+## Structure
+
+The directory `data` contains the data we plan to automatically update. The script we use to retrieve the data shall be `get-data.R`.
+
+Other notebooks are attempts of visualising and manipulating the data.
---
+++ get-data.R
@@ -0,0 +1,47 @@
+library(tidyverse)
+
+# Vaccine Deliveries ####
+
+url_vaccine_deliveries_ita <-
+  'https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/consegne-vaccini-latest.csv'
+
+vaccine_deliveries_ita <- read_csv(url_vaccine_deliveries_ita) %>%
+  mutate(area = as.factor(area))
+
+vaccine_deliveries_ita %>%
+  write.csv('data/ita_vaccine_deliveries.csv')
+
+# Vaccinations Data by Demography ####
+
+url_vaccininations_demographic_data <-
+  'https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/somministrazioni-vaccini-latest.csv'
+
+vaccininations_demographic_data <- read_csv(url_vaccininations_demographic_data) %>%
+  mutate(across(c(area, fascia_anagrafica), as.factor))
+
+vaccininations_demographic_data %>%
+  write.csv('data/ita_vaccinations_demographic_data.csv')
+
+# Aggregate Vaccinations Data ####
+
+vaccinations_aggregated_data <-
+  vaccininations_demographic_data %>%
+  group_by(data_somministrazione, area) %>%
+  summarise(across(where(is.numeric), sum)) %>%
+  mutate(totale = sesso_maschile + sesso_femminile) %>%
+  relocate(totale, .after = area)
+
+vaccinations_aggregated_data %>%
+  write.csv('data/ita_vaccinations_aggregated_data.csv')
+
+# Cumulative Data ####
+
+vaccinations_cumulative_italy <-
+  vaccinations_aggregated_data %>%
+  group_by(area) %>%
+  summarise(totale = sum(totale)) %>%
+  add_row(area = 'ITA',
+          totale = sum(vaccinations_aggregated_data$totale)) 
+
+vaccinations_cumulative_italy %>%
+  write.csv('data/ita_vaccinations_cumulative_total.csv')
